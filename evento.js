77494dca56da5e8a4e327f038360d5e0c2b9f450
Pablo Menezes <pablomenezes670@users.noreply.github.com>
2017-04-12 21:36:44 +0300

    Add files via upload

---
+++ evento.js
@@ -0,0 +1,12 @@
+function evento_entrar() 
+{
+   alert("Seja Bem Vindo");
+}
+function evento_sair()
+{
+    alert("Até Proxima Visita");
+}
+function evento_clique() 
+{
+   window.document.getElementById("id2").style.color="red";
+}
